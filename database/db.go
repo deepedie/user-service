20368150ea70f17d93c7597440a77a82fe2a711c
Jefry <jefryhsinaga@gmail.com>
2024-09-19 11:35:33 +0700

    first commit

--- database/db.go
+++ database/db.go
@@ -16,7 +16,7 @@ var (
 )
 
 func StartDB() {
-	config := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
+	config := fmt.Sprintf("host=%s dbname=%s port=%s sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
 
 	db, err = gorm.Open(postgres.Open(config), &gorm.Config{})
 	if err != nil {
